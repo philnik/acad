2a0de6680538980ca0743b7da861bef1700bd803
Nikolakopoulos Filippos <f.nikolakopoulos@gmail.com>
2024-10-22 23:55:07 +0300

    add_new_style_command

--- file_operations.lisp
+++ file_operations.lisp
@@ -20,6 +20,8 @@
   (invoke (ole (ole doc :Application) :Documents) :Open fname)
   )
 
+(invoke (ole doc :TextStyles) :Add "Standard1")
+
 (defun save_dwg_as (fname)
   (send_command (format nil "(vla-saveas (vla-get-ActiveDocument (vlax-get-Acad-Object)) \"~a\" ) " fname3))
   )
@@ -38,7 +40,29 @@
 		  (send_command str))))
 
 ;(draw_concentric_circles 10)
-
 ;(invoke model :AddMtext "200.0 200.0" 1000d0 "1hellodddd11")
+
 ;(invoke model :AddLine "0.0 0.0" "1.0 0.0")
 
+(defun add_new_style_command ()
+(send_command 
+ #?"
+(defun new_style ()
+(entmake
+(list
+'(0 . \"STYLE\")
+'(100 . \"AcDbSymbolTableRecord\")
+'(100 . \"AcDbTextStyleTableRecord\")
+'(2 . \"Design-Level2\")
+'(70 . 0)
+'(40 . 0.0)
+'(41 . 1.0)
+'(50 . 0.0)
+'(71 . 0)
+'(42 . 0.09375)
+'(3 . \"ArialN.ttf\")
+'(4 . \"\")
+)
+)) 
+"
+)
